0ec538bd92307e58d3f922b96243a7cfcbd2d03d
rn4nv <dorjuan@ya.ru>
2022-12-12 21:06:56 +0700

    Completed CS task 003

---
+++ CS_task_003/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.Clear();
+
+void UnnamedArray(double[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = Math.Round(new Random().NextDouble(),5);
+        index++;
+    }
+}
+
+void PrintArray(double[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+double Diff(double[] collection)
+{
+    int quantity = collection.Length;
+    int index = 0;
+    double max = collection[index];
+    double min = collection[index];
+    double result = 0;
+
+
+    while (index <= quantity)
+    {
+        if (index == quantity)
+        {
+            result = max - min;
+            Console.Write("Разница между максимальным и минимальным элементом массива равно: ");
+            Console.Write(Math.Round(result, 5));
+            break;
+        }
+        else
+        {
+            if (collection[index] > max) max = collection[index];
+            if (collection[index] < min) min = collection[index];
+            index++;
+        }
+
+        
+    }
+    return result;
+    
+
+}
+
+double[] array = new double[4];
+
+UnnamedArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Diff(array);
